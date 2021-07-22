6fc17a93bb86c7be684f02a4f3e423c6cbfb5869
minjilee-03 <56942604+minjilee-03@users.noreply.github.com>
2021-07-22 09:36:04 +0900

    992px slick 반응형 수정중

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-/*main-visual*/
-$(document).ready(function(){
-    $(".main-visual-slick-slides").slick({
-      fade: true,
-      autoplay: false,
-      cssEase: 'ease-in-out',
-      arrows: true
-    });
+$(document).ready(function () {
+  $(".main-visual-slick-slides").slick({
+    fade: true,
+    autoplay: false,
+    cssEase: 'ease-in-out',
+    arrows: true
+  });
+});
+
+$(function () {
+  var $header = $('.header-inner-wrap');
+  var $page = $('.main-visual-txt-con');
+  var $window = $(window);
+  var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
+
+  $window.resize(function () { //반응형을 대비하여 리사이즈시 top값을 다시 계산
+    pageOffsetTop = $page.offset().top;
   });
 
-  $(function(){
-    var $header = $('.header-inner-wrap');
-    var $page = $('.main-visual-txt-con');
-    var $window = $(window);
-    var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
-    
-    $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
-      pageOffsetTop = $page.offset().top;
-    });
-    
-    $window.on('scroll', function(){ //스크롤시
-      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
-      $header.toggleClass('down', scrolled); //클래스 토글
-    });
+  $window.on('scroll', function () { //스크롤시
+    var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+    $header.toggleClass('down', scrolled); //클래스 토글
   });
-/*content2-slider*/
-  $(document).ready(function(){
-    $(".main-development-con").slick({
-      fade: true,
-      autoplay: false,
-      cssEase: 'ease-in-out',
-      arrows: false
-    });
-  });
+});
 
-  $.noConflict();
+$(document).ready(function () {
+  $(".main-development-con").slick({
+    fade: true,
+    autoplay: false,
+    cssEase: 'ease-in-out',
+    arrows: false,
+    responsive: [ // 반응형 웹 구현 옵션
+      {
+        breakpoint: 992, //화면 사이즈 992px
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        }
+      }
+    ]
+  });
+})
+
+$.noConflict();
